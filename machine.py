f16e38b92c55d22682875177e48342187537ca74
VoidKing <adrien.kaet@gmail.com>
2021-03-24 18:08:02 +0100

    starting to remote stuff

--- machine.py
+++ machine.py
@@ -6,6 +6,8 @@ import random as rd
 from time import time
 import matplotlib.pyplot as plt
 
+import ray
+
 # Pour commencer, pour pas trop se prendre la tête, on va fixer firestart constamment à x_max/2, y_max/2
 
 
@@ -37,16 +39,52 @@ def matdiff(a,b):
 	return len(a[a != b])**2
 
 
+# We write a version of gradient that is not a method of machine but a function that takes 
+# all the required arguments
+# This way we can easily make it remote
+
+def fgradient(moisture, value, c_intercept, c_moisture, h):
+
+	x,y = moisture.shape
+
+# 		On calcule d'abord la prédiction sur le paramètre actuel			
+	auto_0 = Automaton(c_intercept, c_moisture, shape = (x,y), firestart = (int(x/2), int(y/2)), moisture = moisture)
+	c_0 = matdiff(auto_0.final_state(), value)
+		
+# 		Puis avec chacun des paramètres augmenté de self.h
+# 		On peut ensuite calculer la dérivée partielle par rapport à chaque paramètre et actualiser le gradient
+
+# 		c_intercept + h d'abord
+	auto_i = Automaton(c_intercept + h, c_moisture, shape = (x,y), firestart = (int(x/2), int(y/2)), moisture = moisture)
+	c_i = matdiff(auto_i.final_state(), value)
+	g_i = (c_i - c_0)/h
+			
+# 		c_moisture + h ensuite
+	auto_m = Automaton(c_intercept, c_moisture + h, shape = (x,y), firestart = (int(x/2), int(y/2)), moisture = moisture)
+	c_m = matdiff(auto_m.final_state(), value)
+	g_m = (c_m - c_0)/h
+		
+	return g_i, g_m
+
+
+remote_fgradient = ray.remote(fgradient)
+
+
 
 class machine:
 	
-	def __init__(self, data, mb_size, c_intercept = -10, c_moisture = 20, h = 1, learning_rate = 0.000001):
+	def __init__(self, data, mb_size, c_intercept = -10, c_moisture = 20, h = 1, learning_rate = 0.000001, remote = False):
+		
+		if remote and (not ray.is_initalized):
+			ray.init(address = "auto")
+		
 		self.data = data
 		self.mb_size = mb_size
 		self.c_intercept = c_intercept
 		self.c_moisture = c_moisture
 		self.h = h
 		self.learning_rate = learning_rate
+		self.remote = remote
 	
 	
 	def __str__(self):
@@ -106,9 +144,15 @@ class machine:
 		
 		return g_i, g_m
 
-
 	
 	def learn_step(self):
+		if self.remote:
+			self.remote_learn_step()
+		else:
+			self.normal_learn_step()
+	
+	
+	def normal_learn_step(self):
 		
 # 		First we get a subset of mb_size element of data
 # 		We randomly select rows, and each row might get selected more than once
@@ -136,6 +180,36 @@ class machine:
 		self.c_intercept -= grad_intercept
 		self.c_moisture -= grad_moisture
 
+	
+	def remote_learn_step(self):
+		
+# 		Ray was already initialized in the constructor if ray.remote
+# 		so we don't need to take care of it here
+		
+		n = len(self.data)
+		l = [k for k in range(n)]
+		ll = rd.choices(l, k = self.mb_size)
+		
+		grad_intercept = 0
+		grad_moisture = 0
+		
+# 		Instead of computing gradient on a data point and incrementing the gradients directly,
+# 		we ask for a remote computation of all the gradients and we increment once the
+# 		computation is over
+		
+		futures = [remote_fgradient.remote(self.data.iloc[k]['moisture'], self.data.iloc[k]['value'], self.c_intercept, self.c_moisture, self.h) for k in range(ll)]
+		grad_list = ray.get(futures)
+		
+		for g_i, g_m in grad_list:
+			grad_intercept += g_i
+			grad_moisture += g_m		
+		
+		grad_intercept = (grad_intercept/self.mb_size)*self.learning_rate
+		grad_moisture = (grad_moisture/self.mb_size)*self.learning_rate
+		
+		self.c_intercept -= grad_intercept
+		self.c_moisture -= grad_moisture
+		
 	
 	def learning(self,n):
 		for k in range(n):
@@ -166,3 +240,5 @@ for k in range(50):
 	print("Cost: {}".format(daneel.fullcost()))
 	print(daneel)
 
+if ray.is_initalized():
+	ray.shutdown()
